9a45307d10ec7631f3e71b6e388ff4a4a3079e79
Andrew Spyker <awspyker@gmail.com>
2015-05-19 14:35:13 -0700

    using annotations

--- simple-sample/src/main/java/com/netflix/archaius/samples/simple/SimpleSample.java
+++ simple-sample/src/main/java/com/netflix/archaius/samples/simple/SimpleSample.java
@@ -7,9 +7,14 @@ import com.netflix.archaius.guice.ArchaiusModule;
 public class SimpleSample {
     public static void main(String args[]) {
         Injector injector = Guice.createInjector(new ArchaiusModule());
+
         SomeService ss = injector.getInstance(SomeService.class);
         ss.getDirectly();
         ss.getAgainstProperty();
         ss.changePropertyThatIsMonitored();
+
+        SomeService2 ss2 = injector.getInstance(SomeService2.class);
+        ss2.getDirectly();
+        ss2.getAgainstProperty();
     }
 }
---
+++ simple-sample/src/main/java/com/netflix/archaius/samples/simple/SomeService2.java
@@ -0,0 +1,40 @@
+package com.netflix.archaius.samples.simple;
+
+import com.netflix.archaius.Config;
+import com.netflix.archaius.DefaultPropertyFactory;
+import com.netflix.archaius.Property;
+import com.netflix.archaius.PropertyListener;
+import com.netflix.archaius.annotations.Configuration;
+import com.netflix.archaius.config.CompositeConfig;
+import com.netflix.archaius.config.DefaultSettableConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+@Configuration
+public class SomeService2 {
+    private static Logger logger = LoggerFactory.getLogger(SomeService2.class);
+    private int intprop;
+    private Property<String> stringprop;
+    private boolean boolprop;
+
+    @Inject
+    public SomeService2() {
+    }
+
+    public void getDirectly() {
+        logger.debug("intprop = " + intprop);
+    }
+
+    public void getAgainstProperty() {
+        logger.debug("stringprop = " + stringprop.get());
+    }
+
+    public void setBoolprop(Boolean boolprop) {
+        logger.debug("boolprop being set to " + boolprop);
+        this.boolprop = boolprop;
+    }
+}
